95d94a0b15b57ff5003183d6a8f17df8ba3c79a4
ewsbdli <mahaabdaljalil2@gmail.com>
2026-04-21 13:06:44 +0200

    Enable Speed Insights

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import { injectSpeedInsights } from '@vercel/speed-insights';
+ 
+injectSpeedInsights();
 import "./style.css";
 import { loginWithGoogle } from "./services/firebase";
 import { initChessUI } from "./ui/events";
